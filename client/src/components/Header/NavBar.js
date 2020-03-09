91194cfb982f67d43636c00056c8e46224cb1447
Brock Major <brockmajor@gmail.com>
2020-03-09 15:44:13 -0400

    top bar, edited nav bar, added some redirects from nav bar

--- client/src/components/Header/NavBar.js
+++ client/src/components/Header/NavBar.js
@@ -5,20 +5,12 @@ import './NavBar.css';
 const NavBar = () => {
     return (
         <div className = "header">
-            {/* Logo */}
-            <Link className = "nav-title" to="/">
-                <img className = "nav-logo" src={ "/logo192.png" } alt="React logo" />
-            </Link>
-
             {/* Page Links */}
             <div className = "nav-items">
-                <Link className = "nav-link" to='/Home'>Home</Link>
-                <Link className = "nav-link" to='/Register'>Extra Page</Link>
-                <a className = "nav-link" target='_blank' rel="noopener noreferrer" href="https://reactjs.org/docs/getting-started.html">
-                    React Docs
-                </a>
-                <a className = "nav-link" target="_blank" rel="noopener noreferrer" href="https://reactjs.org/tutorial/tutorial.html">React Tutorial</a>
-                <a className = "nav-link" target="_blank" rel="noopener norefferer" href="https://nodejs.org/en/docs/">Node Docs</a>
+                <Link className = "nav-link" to='/home'>Home</Link>
+                <Link className = "nav-link" to='/about'>About Us</Link>
+                <Link className = "nav-link" to='/map'>Map</Link>
+                <Link className = "nav-link" to='/admin'>Admin</Link>
             </div>
 
         </div>
--- client/src/views/Home/Home.js
+++ client/src/views/Home/Home.js
@@ -1,18 +1,28 @@
 import React from 'react';
 import logo from '../../assets/logo.svg';
+import NavBar from "../../components/Header/NavBar";
+import Options from "../../components/Options";
+import Services from "../../components/Services";
 import './Home.css';
 
 function Home() {
     return (
         <div className="App">
+            <input type="text" placeholder="Search"/>
+            <div className="head">
+            <h1> Welcome to City Traffic </h1>
+            </div>
+            <NavBar />
+            <div className="description">
+                <h2>
+                    This web app's function is to help the user compare
+                    traffic and accident reports from different cities.
+                    It also provides information for insurance and weather reports,
+                    as well as driving tests to earn stars for insurance discounts.
+                </h2>
+            </div>
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Accident Web App
-                </p>
-                <p>
-                  <i> Created by  Grant Boicheff, Kareem Joudeh, Brock Major, Matthew Petela, Ethan Sodikromo, Elijah Springer, & Adam Van Belkum </i>
-                </p>
                 <a
                     className="App-link"
                     href="https://reactjs.org"
