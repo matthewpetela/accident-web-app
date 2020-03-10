3b14f434fdf4f352428956f42518c0462cb18b4e
Matthew Petela <matt@matthewpetela.com>
2020-03-10 03:43:24 -0400

    Added About & Map pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Route, Switch, Redirect  } from 'react-router-dom';
 import Home from "./views/Home/Home";
+import Map from "./views/Map/Map";
+import About from "./views/About/About";
 import NotFound from "./views/NotFound";
 
 const App = () => {
@@ -11,6 +13,8 @@ const App = () => {
         <Route exact path="/">
           <Redirect to="/home" />
         </Route>
+        <Route exact path="/map" component={Map} />
+        <Route exact path="/about" component={About} />
         <Route component={NotFound}/>
       </Switch>
     </div>
---
+++ client/src/views/About/About.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import NavBar from "../../components/Header/NavBar";
+import './About.css';
+
+function About(){
+  return (
+    <div className='App'>
+      <inout type='text' placeholder="Search"/>
+      <div className='head'>
+        <h1> City Traffic </h1>
+      </div>
+        <NavBar/>
+      </div>
+  )
+}
+
+export default About;
