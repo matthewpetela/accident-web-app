7cf0ee318db410e184a0ebd964df4c646d074b67
ESodikromo <ethan.sodikromo@gmail.com>
2020-03-10 15:49:10 -0400

    Hotfix jank weather api call, change map.js localhost to appropriate url later

--- client/src/views/Map/Map.js
+++ client/src/views/Map/Map.js
@@ -1,9 +1,27 @@
-import React from 'react';
+import React ,{useEffect, useState} from 'react';
 import logo from '../../assets/icons/logo.png';
 import NavBar from "../../components/Header/NavBar";
+import request from 'request'
 import './Map.css';
 
 function Map(){
+
+    var [weatherData, setWeatherData] = useState('Blank');
+
+    useEffect(() => {
+        // Update the document title using the browser API
+        request({
+            url: 'http://localhost:3000//api/weather/test', //HARDCODED TO NEW YORK CITY
+        }, async (error, response, body) => {
+            if (error) {
+                setWeatherData('Error');
+                console.log(error);
+            }else{
+                setWeatherData(body);
+            }
+        });
+    },[]);
+
   return (
     <div className='App'>
       <inout type='text' placeholder="Search"/>
@@ -11,8 +29,7 @@ function Map(){
         <h1> City Traffic </h1>
       </div>
       <NavBar/>
-
-
+        <p>{weatherData}</p>
 
     </div>
   )
--- server/controllers/server.WeatherController.js
+++ server/controllers/server.WeatherController.js
@@ -3,7 +3,7 @@ var request = require('request');
 
 
 //export const report = (req,res)=> {
-const report = (req,res)=> {
+exports.report = (req,res)=> {
     if (!req) {
         res.status(400).send({
             message: " Empty!"
@@ -12,12 +12,12 @@ const report = (req,res)=> {
     request({
         url: 'https://api.darksky.net/forecast/0f8e93980168dda45bd651f9c01a18a7/40.7128,-74.0060', //HARDCODED TO NEW YORK CITY
     }, async (error, response, body) => {
-        if (error) res.send(error);
-        let weather = body;
-        //format data here
-        res.send(weather);
-
+        if (error){
+            res.send(error);
+        }else {
+            let weather = body;
+            //format data here
+            res.send(weather);
+        }
     });
 };
-
-module.exports = report;
