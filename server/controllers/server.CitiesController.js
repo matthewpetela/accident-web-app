b952613769dcf82609c5a42f765ed0136f1469aa
ESodikromo <ethan.sodikromo@gmail.com>
2020-04-17 03:43:01 -0400

    Obtained and routed city data, and it is done. Actual historical weather data and the algo to find neighbors in cityController. John 19:30

--- server/controllers/server.CitiesController.js
+++ server/controllers/server.CitiesController.js
@@ -1,4 +1,4 @@
-
+const fs = require('fs');
 //import City from '../models/server.CityModel.js';
 var City = require("../models/server.CityModel.js");
 exports.city = function(req, res) {
@@ -12,13 +12,27 @@ exports.city = function(req, res) {
 };
 
 exports.cities = function(req, res) {
-    let cityArr= {};
-    City.find({}, (err, docs) => {
-        if (err) {
-            res.send('we cant find them(ALL)');
-        }else{
-            cityArr = docs;
+    let cityArr= JSON.parse(fs.readFileSync(process.cwd()+'\\server\\adminData\\cityData.JSON'));
+    //console.log(cityArr[0]['avgTemp']);
+    res.send(cityArr);
+    for(x in cityArr){
+        var compArr = [];
+        for(y in cityArr){
+            var prod = 1;
+            for(i = 0; i< 12; i++){
+                prod= prod*Math.abs(cityArr[x]['avgTemp'][i] - cityArr[y]['avgTemp'][i]);
+            }
+            process.stdout.write(prod.toExponential(2).toString()+' ');
+            compArr.push(prod);
         }
-
-    }).then(res.send(cityArr));
+        var sorted = compArr.slice().sort(function(a,b){return b-a})
+        var ranks = compArr.slice().map(function(v){ return -1*(sorted.indexOf(v)-7)});
+        var names =[];
+        for(g in ranks){
+            if(ranks[g]<4 && ranks[g] !=0){
+                names.push(cityArr[g]['city'])
+            }
+        }
+        console.log(names);
+    }
 };
