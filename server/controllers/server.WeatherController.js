2acf18325748bd880f1c1ce5e6cf398ee8a82e24
ESodikromo <ethan.sodikromo@gmail.com>
2020-04-17 03:48:38 -0400

    Obtained and routed city data, and it is done. Actual historical weather data and the algo to find neighbors in cityController. John 19:30

--- server/controllers/server.WeatherController.js
+++ server/controllers/server.WeatherController.js
@@ -1,23 +1,24 @@
 //import request from 'request';
-var request = require('request');
-
+const axios =require("axios");
+const fs = require('fs');
+let cityArr= JSON.parse(fs.readFileSync(process.cwd()+'\\server\\adminData\\cityData.JSON'));
 
 //export const report = (req,res)=> {
 exports.report = (req,res)=> {
+    const found = cityArr.find(element => element['city'] === req.params.cityName);
     if (!req) {
         res.status(400).send({
             message: " Empty!"
         });
     }
-    request({
-        url: 'https://api.darksky.net/forecast/0f8e93980168dda45bd651f9c01a18a7/40.7128,-74.0060', //HARDCODED TO NEW YORK CITY
-    }, async (error, response, body) => {
-        if (error){
-            res.send(error);
-        }else {
-            let weather = body;
-            //format data here
-            res.send(weather);
-        }
-    });
+    console.log(found['coordinates'][0]);
+    axios
+        .get('https://api.darksky.net/forecast/0f8e93980168dda45bd651f9c01a18a7/'+found['coordinates'][0]+','+found['coordinates'][1], this.state)
+        .then(response => {
+            res.send(response.data);
+        })
+        .catch(error => {
+            console.log(error)
+            res.status(500).send(error);
+        });
 };
