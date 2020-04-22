dad034bd3d0dd93747a80acc3ff3634a00af38d5
Matthew Petela <matt@matthewpetela.com>
2020-04-21 22:36:46 -0400

    Might have fixed quiz

--- client/src/views/Quiz/QuizResult.js
+++ client/src/views/Quiz/QuizResult.js
@@ -12,68 +12,69 @@ const QuizResult = (props) => {
         //request.post("https://accident-web-app.herokuapp.com/api/upload/userQuizResults", quizGrade)
         const loginDetails = localStorage.getItem('token')
         var accountDetails
+        console.log(quizGrade)
         axios
-        .post(process.env.NODE_ENV === 'production'?'https://accident-web-app.herokuapp.com/api/users/userAccount':'http://localhost:5000/api/users/userAccount',
-        { data: {quizGrade} })
+        .post(process.env.NODE_ENV === 'production'?'https://accident-web-app.herokuapp.com/api/upload/userQuizResults':'http://localhost:5000/api/upload/userQuizResults',
+        { data: {"quizGrade" : quizGrade} }, {headers: {"Authorization" : `bearer ${localStorage.getItem("token")}`}})
         .then(res=>{
-          console.log(res);         
+          console.log(res);
         })
         .catch(error => {
           console.log(error)
-        })        
+        })
     }
 
     const questionBox = {
-        backgroundColor: "royalblue",        
+        backgroundColor: "royalblue",
         display: 'inline-block',
         borderRadius: '20px',
         width: '50vw',
         height: '58vh',
         margin: 'auto',
-        border: '2px solid',    
+        border: '2px solid',
     }
     const innerDiv = {
-        margin: 'calc(3vw + 3vh)'        
-    }    
+        margin: 'calc(3vw + 3vh)'
+    }
     const questionText = {
         width: '90%',
-        height: '90%',        
-        margin: "auto",        
+        height: '90%',
+        margin: "auto",
         fontSize: '3.2vmin',
         textAlign: 'center',
-        color: 'black'        
+        color: 'black'
     }
     const questionTextContainer = {
         backgroundColor: 'white',
         borderRadius: '20px',
-        marginBottom: '8vh',        
-        
-    }   
+        marginBottom: '8vh',
+
+    }
     const imageStyling = {
         width: '15vmin',
         height: '15vmin',
-    } 
+    }
     if(quizGrade >= 100)
     {
         saveQuizGrade()
-        return (                
+        return (
             <div style={questionBox}>
                 <div style={innerDiv}>
                     <div style={questionTextContainer}><h3 style={questionText}>You scored a {quizGrade}% and earned a gold star.</h3></div>
-                    <img style={imageStyling} src={GoldStar}/>          
-                </div>                 
+                    <img style={imageStyling} src={GoldStar}/>
+                </div>
             </div>
-        ); 
+        );
     }
     else
     {
         saveQuizGrade()
-        return (                
+        return (
             <div style={questionBox}>
                 <div style={innerDiv}>
                     <div style={questionTextContainer}><h3 style={questionText}>You scored a {quizGrade}% and didn't earn any stars.</h3></div>
-                    <img style={imageStyling} src={Fail}/>          
-                </div>                 
+                    <img style={imageStyling} src={Fail}/>
+                </div>
             </div>
         );
     }
